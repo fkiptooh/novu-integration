e192799e8ebf3b16488e13ec19db64653c529ab9
Felix Kiptoo <fkiptooh@gmail.com>
2024-09-15 13:12:28 +0300

    keys: novu_bridge_ur

--- app/api/novu/route.ts
+++ app/api/novu/route.ts
@@ -1,7 +1,7 @@
 import { serve } from "@novu/framework/next";
 import { welcomeOnboardingEmail } from "../../novu/workflows";
 
-// the workflows collection can hold as many workflow definitions as you need
+// the workflows collection can hold as many workflow definitions as you need added keys
 export const { GET, POST, OPTIONS } = serve({
   workflows: [welcomeOnboardingEmail],
 });
